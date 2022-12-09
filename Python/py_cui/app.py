7181a0f8f0f1e7a96bc4ac40db37d39e9d9bad63
Sein-Oh <48703174+Sein-Oh@users.noreply.github.com>
2022-12-10 01:41:35 +0900

    Update app.py

--- Python/py_cui/app.py
+++ Python/py_cui/app.py
@@ -6,22 +6,32 @@ from StringProgressBar import progressBar
 class App:
     def __init__(self, root: py_cui.PyCUI):
         self.root = root
-
-        self.idx = 0
+        self.root.set_title("Lineage-W by MANDLOH")
+        self.root.set_refresh_timeout(1)
         self.root.set_widget_border_characters("*", "*", "*", "*", "-", "|")
         # self.root.toggle_unicode_borders()
 
-        self.figlet = self.root.add_block_label(art.text2art("MANDLOH", "rectangles"), 0, 0, column_span=10, row_span=5)
-        self.figlet.set_selectable(False)
-        self.figlet.set_color(0)
+        # self.figlet = self.root.add_block_label(art.text2art("MANDLOH", "rectangles"), 0, 3, column_span=7, row_span=3)
+        # self.figlet.set_selectable(False)
+        # self.figlet.set_color(py_cui.MAGENTA_ON_BLACK)
 
-        self.hp = self.root.add_label(self.progress_bar("HP",100,100,20), 7, 1, column_span=3)
+        self.hp = self.root.add_label(self.progress_bar("HP",100,100,20), 0, 0, column_span=3)
         self.hp.set_selectable(False)
         self.hp.add_text_color_rule("=", py_cui.WHITE_ON_RED, "contains", match_type='regex')
 
-        self.mp = self.root.add_label(self.progress_bar("MP",100,99,20), 8, 1, column_span=3)
+        self.mp = self.root.add_label(self.progress_bar("MP",100,99,20), 1, 0, column_span=3)
         self.mp.set_selectable(False)
         self.mp.add_text_color_rule("=", py_cui.WHITE_ON_BLUE, "contains", match_type='regex')
+
+        self.run = self.root.add_label("RUN", 0, 3)
+        self.run.add_mouse_command(py_cui.keys.LEFT_MOUSE_CLICK, lambda: self.toggle_event(self.run))
+        self.style_to_button(self.run)
+
+        self.help = self.root.add_label("HELP", 0, 4)
+        self.help.add_mouse_command(py_cui.keys.LEFT_MOUSE_CLICK, lambda: self.toggle_event(self.help))
+        self.style_to_button(self.help)
+
+        self.info = self.root.add_label("EMPTY", 8, 0)
         
         # self.root.add_text_block("Use", 5, 1, column_span=1, row_span=6)
         # self.root.add_text_block("Main", 6, 1, column_span=5, row_span=6)
@@ -47,6 +57,20 @@ class App:
         self.input.add_mouse_command(py_cui.keys.LEFT_MOUSE_CLICK, lambda: self.root.move_focus(self.input))
         self.root.add_key_command(py_cui.keys.KEY_A_LOWER, self.test)
         """
+    def style_to_button(self, widget):
+        widget.value = False
+        widget.set_color(py_cui.BLACK_ON_WHITE)
+
+    def toggle_event(self, widget):
+        widget.value = not widget.value
+        widget.set_color(py_cui.BLACK_ON_GREEN if widget.value else py_cui.BLACK_ON_WHITE)
+
+    def change_info(self, title, msg):
+        self.info.set_title(f"{title}-{msg}")
+
+
+    def run_evt(self, x, y):
+        self.run.set_color(py_cui.BLACK_ON_GREEN if self.run.value else py_cui.BLACK_ON_WHITE)
 
     def test(self):
         selected = self.menu.get_selected_item_index()
@@ -59,9 +83,9 @@ class App:
 
     def progress_bar(self, title, total, current, size):
         bar, value = progressBar.filledBar(total, current, size, "-", "=")
-        return f"{title}:{bar} {str(int(float(value))).rjust(3)}%"
+        return f"{title} {bar} {str(int(float(value))).rjust(3)}%"
         
 
-root = py_cui.PyCUI(26,10)
+root = py_cui.PyCUI(24,10)
 App(root)
 root.start()
