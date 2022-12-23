e8378a89f9715ccb3e3d445a3e70f71e2564b4a7
Sein-Oh <48703174+Sein-Oh@users.noreply.github.com>
2022-12-23 16:24:10 +0900

    Update app.py

--- Python/cli/app.py
+++ Python/cli/app.py
@@ -2,7 +2,8 @@ from pytimedinput import timedKey
 from InquirerPy import inquirer
 import os
 import json
-from rich import print
+import msvcrt
+import time
 
 def load_data(name):
     with open(name, "r") as f:
@@ -14,23 +15,38 @@ def select_slot():
 def select_key():
     return inquirer.rawlist(message="Value to change:", choices=["USE", "HP_MIN", "HP_MAX", "MP_MIN", "MP_MAX", "KEY", "COUNT", "COOLTIME", "REPEAT"]).execute()
 
-data = load_data("user_data.json")
+def clear():
+    os.system("cls")
+    print("START")
 
+def main_log(msg):
+    now = time.localtime()
+    text = f"[{now.tm_mon}-{now.tm_mday} {now.tm_hour}:{now.tm_min}:{str(now.tm_sec).rjust(2, '0')}] : {msg}"
+    print(text, end="")
+    print("\b" * len(text), end="", flush=True)
+
+data = load_data("user_data.json")
+clear()
 
 while True:
-    msg = f"[red]HP:100[/red] / [blue]MP:100[/blue]"
-    print(msg, end="")
-    text, timeout = timedKey("", 0.2)
-    if not timeout:
-        os.system("cls")
+    main_log("HELLO WORLD!!")
+    if msvcrt.kbhit():
+        clear()
         select = inquirer.rawlist(message="Select menu:", choices=["Change setting", "BACK"]).execute()
         if select == "Change setting":
             slot = select_slot()
             print("Current your values.")
-            print(data[slot])
+            print("==========================")
+            for key, value in data[slot].items():
+                print(key.ljust(8), ":", value)
+            print("==========================")
+            
             key = select_key()
             if key == "USE" or "REPEAT":
                 data[slot][key] = not data[slot][key]
                 print(f"{slot}-{key} : {data[slot][key]}")
+                time.sleep(2)
+        clear()
+    time.sleep(0.2)
 
 
