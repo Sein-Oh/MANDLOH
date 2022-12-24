d2d2362604825b8d25ba92039aa887f32f2008b3
Sein-Oh <48703174+Sein-Oh@users.noreply.github.com>
2022-12-24 09:10:14 +0900

    Update app.py

--- Python/textual/app.py
+++ Python/textual/app.py
@@ -1,7 +1,7 @@
 from textual.app import App
 from textual.containers import Horizontal
 from textual.reactive import reactive
-from textual.widgets import Static, Button, TextLog, Label
+from textual.widgets import Static, Button, TextLog, Checkbox, Input
 from textual.widget import Widget
 from StringProgressBar import progressBar
 
@@ -13,46 +13,41 @@ class TitleLog(Static):
 class Monitor(Static):
     def compose(self):
         yield Static(self.renderable, classes="title")
-        yield Static(id="hp")
-        yield Static(id="mp")
+        yield Static("")
+        self.hp = Static()
+        self.mp = Static()
+        yield self.hp
+        yield self.mp
         yield Static("")
 
-class BoolLabel(Widget):
-    title = reactive("TITLE")
-    value = reactive(False)
-    
-    def render(self):
-        return f"{self.title.rjust(8)}:{self.value}"
-    
-    def on_click(self):
-        self.value = not self.value
+    def on_mount(self):
+        self.update_value(100, 100)
 
+    def update_value(self, hp, mp):
+        self.hp.update(f"  HP [{str(hp).rjust(3)}%] [[red]{progressBar.filledBar(100, hp, 30, ' ', '=')[0]}[/red]]")
+        self.mp.update(f"  MP [{str(mp).rjust(3)}%] [[blue]{progressBar.filledBar(100, mp, 30, ' ', '=')[0]}[/blue]]")
+
+class Check(Static):
+    def compose(self):
+        yield Static(self.renderable, classes="label")
+        yield Checkbox()
 
 class MyApp(App):
     CSS_PATH = "app.css"
 
     def compose(self):
-        yield Horizontal(Monitor("STATUS"), TitleLog("MESSAGE") , classes="box")
-        yield Button("TEST")
+        yield Horizontal(Monitor("  STATUS"), TitleLog("  MESSAGE") , classes="box")
+        yield Button("TEST", id="test")
+        yield Check("USE")
 
-    def on_mount(self):
-        self.update_monitor(30, 80)
 
-    def on_click(self, event):
-        self.query_one("TitleLog > TextLog").write(event)
+    # def on_click(self, event):
+    #     self.query_one("TitleLog > TextLog").write(event)
 
     def on_button_pressed(self, event):
-        self.query_one("TitleLog > TextLog").write(event)
-        self.update_monitor(90, 90)
+        # self.query_one("TitleLog > TextLog").write(event)
+        if event.button.id == "test":
+            self.query_one(Monitor).update_value(90, 90)
 
-    def make_progress(self, value):
-        return progressBar.filledBar(100, value, 30, " ", "=")[0]
-
-    def update_monitor(self, hp, mp):
-        hpBar = f"HP [{str(hp).rjust(3)}%] [[red]{self.make_progress(hp)}[/red]]"
-        mpBar = f"MP [{str(mp).rjust(3)}%] [[blue]{self.make_progress(mp)}[/blue]]"
-        self.query_one("Monitor > #hp").update(hpBar)
-        self.query_one("Monitor > #mp").update(mpBar)
-
-
-MyApp().run()
+app = MyApp()
+app.run()
