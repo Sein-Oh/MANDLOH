6065ef80cf4f2d78844d1ef6cc83997e2413c8df
Sein-Oh <48703174+Sein-Oh@users.noreply.github.com>
2024-04-01 22:12:43 +0900

    Update temp.py

--- Python/py_cui/temp.py
+++ Python/py_cui/temp.py
@@ -1,46 +1,59 @@
 import py_cui
+import time
+import threading
+
+"""
+button.set_color(int) --> border
+0 : white
+1 : white
+2 : yellow
+3 : red
+4 : sky
+5 : purple
+6 : green
+7 : blue
+"""
 
 class App:
     def __init__(self, root: py_cui.PyCUI):
         self.root = root
-        self.root.add_key_command(py_cui.keys.KEY_C_LOWER, self.key_event)
+        self.root.set_title("MANDLOH")
+        self.root.set_refresh_timeout(0.1)
+        self.root.toggle_unicode_borders()
+
+        self.btn = self.root.add_button("TEST", 0, 0, command=self.test)
+
+        self.log_box = self.root.add_text_block("CONSOLE", 5, 0, row_span=5, column_span=5)
+        self.log_box.set_selectable(False)
+        self.log_box_ary = ["" for x in range(7)]
+
+        self.log("App started.")
+        self.count = 0
+        # threading.Thread(target=self.upcount, daemon=True).start()
+
+    def log(self, msg):
+        now = time.localtime()
+        text = f"[{str(now.tm_hour).zfill(2)}:{str(now.tm_min).zfill(2)}:{str(now.tm_sec).zfill(2)}] : {msg}"
+        self.log_box_ary.pop(0)
+        self.log_box_ary.append(text)
+        log_msg = ""
+        for msg in self.log_box_ary:
+            log_msg += msg + "\n"
+        self.log_box.set_text(log_msg)
+
+    def test(self):
+        self.root.show_menu_popup("MENU", ["A", "B", "C"], command=self.select_menu)
+
+    def select_menu(self, result):
+        self.log(result)
+
+    def upcount(self):
+        while True:
+            self.log(self.count)
+            self.count += 1
+            time.sleep(0.1)
 
 
-        self.menu = self.root.add_scroll_menu("MONITOR", 0, 0)
-        self.menu.add_item_list(["Item1", "Item2", "Item3"])
-        self.menu.add_key_command(py_cui.keys.KEY_ENTER, self.key_event)
-        self.menu.add_mouse_command(py_cui.keys.LEFT_MOUSE_DBL_CLICK, self.show_text_box)
-        self.menu.set_selected_color(py_cui.WHITE_ON_BLUE)
-        self.menu.set_focus_border_color(5)
-
-
-        self.box = self.root.add_text_block("EDITOR", 0, 1)
-        self.box.set_selected_color(py_cui.WHITE_ON_BLUE)
-        self.box.set_focus_border_color(5)
-
-        self.info = self.root.add_label("CHANGE", 1, 0, column_span=2)
-
-    def key_event(self):
-        self.info.set_title(self.menu.get())
-        self.root.set_selected_widget(1)
-
-    def mouse_event(self, x, y):
-        self.info.set_title(self.menu.get())
-        self.menu.add_item_list(["Item4"])
-
-    def set_title(self, title):
-        self.info.set_title(title)
-
-    def show_text_box(self):
-        self.root.show_text_box_popup("Enter a new value.", lambda x:self.change_list(self.menu, x))
-
-    def change_list(self, widget, value):
-        items = widget.get_item_list()
-        items[items.index(widget.get())] = value
-        widget.clear()
-        widget.add_item_list(items)
-
-
-root = py_cui.PyCUI(2,2)
+root = py_cui.PyCUI(10,5)
 App(root)
 root.start()
