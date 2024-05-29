0690d0a01a3293c929d1a9ba636fd77781dcf3b9
Sein-Oh <48703174+Sein-Oh@users.noreply.github.com>
2024-05-29 17:03:19 +0900

    Update app.py

--- Python/textual/app.py
+++ Python/textual/app.py
@@ -1,5 +1,5 @@
 from textual.app import App
-from textual.widgets import Header, Footer, Switch, Static, Button, Log
+from textual.widgets import Header, Footer, Switch, Static, Button, Log, Input
 from textual.containers import Horizontal
 import win32gui
 
@@ -7,24 +7,32 @@ import win32gui
 app_hwnd = win32gui.GetForegroundWindow()
 
 # Set window size and position(Windows only. Right side)
-win32gui.MoveWindow(app_hwnd, 0, 0, 300, 600, True)
+win32gui.MoveWindow(app_hwnd, 0, 0, 400, 600, True)
+
+class MyInput(Static):
+    def compose(self):
+        yield Horizontal(
+            Static(self.renderable),
+            Input()
+        )
+
 
 class MyButton(Static):
     def compose(self):
         yield Horizontal(
             Button(self.renderable, variant="success"),
-            Static("Text", classes="btn_label")
+            Static("Text\nText2\nText3")
         )
 
 class MyApp(App):
     def compose(self):
-        yield Button("Timer 1", id="timer1")
-        yield Button("Timer 2", id="timer2", variant="default")
+        yield MyButton("Timer 1")
+        yield MyButton("Timer 2")
         yield MyButton("Timer 3")
+        yield MyInput("MYINPUT")
         yield Log(id="log")
 
     def on_button_pressed(self, event):
-        # btn_id = event.button.id
         variant = event.button.variant
         event.button.variant = "success" if variant == "default" else "default"
         self.query_one("#log").write_line("Hello world")
