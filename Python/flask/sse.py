05d902d88c8f201f9d1fd7378cc95a62f126cae7
Sein-Oh <48703174+Sein-Oh@users.noreply.github.com>
2024-07-15 22:27:32 +0900

    Update sse.py

--- Python/flask/sse.py
+++ Python/flask/sse.py
@@ -22,5 +22,24 @@ def generator(user_id):
 def connection(user_id: str):
     return Response(generator(user_id), content_type="text/event-stream")
 
+
+@app.get("/")
+def main():
+    return """
+<!DOCTYPE html>
+<html>
+<script>
+    const eventSource = new EventSource("http://localhost:5000/connection/tiaz");
+    eventSource.onmessage = function (event) {
+        console.log("New event:", event.data);
+    };
+    eventSource.addEventListener("notice", (e) => {
+        console.log(event.data);
+    });
+</script>
+
+</html>
+"""
+
 if __name__ == "__main__":
     app.run(port=5000)
