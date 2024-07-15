524cb756adf5d442a53f1584b85a65c7b6437605
Sein-Oh <48703174+Sein-Oh@users.noreply.github.com>
2024-07-15 22:20:56 +0900

    Update sse.py

--- Python/flask/sse.py
+++ Python/flask/sse.py
@@ -1,45 +1,26 @@
 from flask import Flask, Response
-from datetime import datetime
+from flask_cors import CORS
 import time
+import json
+from datetime import datetime
 
 app = Flask(__name__)
+CORS(app)
 
 
-# a generator with yield expression
-def gen_date_time():
+def generator(user_id):
+    #yield f"data: Hello! {user_id}\n\n"
     while True:
-        time.sleep(1)
-        now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        # DO NOT forget the prefix and suffix
-        yield 'data: %s\n\n' % now
+        clock = datetime.now().time()
+        t = f"{clock:%T}"
+        data = {"name": user_id, "time": t}
+        yield f"""event: notice\ndata: {json.dumps(data)}\n\n"""
+        print("Sent")
+        time.sleep(0.5)
 
+@app.get("/connection/<user_id>")
+def connection(user_id: str):
+    return Response(generator(user_id), content_type="text/event-stream")
 
-@app.route('/sse_demo')
-def sse_demo():
-    return Response(
-        gen_date_time(),  # gen_date_time() is an Iterable
-        mimetype='text/event-stream'  # mark as a stream response
-    )
-
-
-HTML = '''<!DOCTYPE html>
-<html>
-<body>
-    Server side clock: <span id="clock"></span>
-    <script>
-        var source = new EventSource("/sse_demo");
-        source.onmessage = function (event) {
-            document.getElementById("clock").innerHTML = event.data;
-            console.log(event.data);
-        };
-    </script>
-</body>
-</html>'''
-
-
-@app.route('/')
-def index():
-    return HTML
-
-
-app.run()
+if __name__ == "__main__":
+    app.run(port=5000)
