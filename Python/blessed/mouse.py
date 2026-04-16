4b053689c0a65685b8d1ce734299f59eb474befc
Sein-Oh <48703174+Sein-Oh@users.noreply.github.com>
2026-04-16 23:00:53 +0900

    Update mouse.py

--- Python/blessed/mouse.py
+++ Python/blessed/mouse.py
@@ -1,25 +1,31 @@
-#!/usr/bin/env python
 from blessed import Terminal
 import time
 term = Terminal()
 
-if not term.does_mouse(report_motion=True):
-    print("This terminal does not support mouse motion tracking!")
-else:
-    # Track current color for painting
-    color_idx = 7
-    num_colors = min(256, term.number_of_colors)
-    header = "Mouse wheel sets color=[{0}], LEFT button paints, RIGHT erases, ^C:quit"
+slots = {
+    'slot1': False,
+    'slot2': False,
+    'slot3': False,
+    'slot4': False
+}
+print(term.clear)
 
-    def make_header():
-        return term.home + term.center(header.format(term.color(color_idx)('█')))
-    text = make_header()
+with term.cbreak(), term.hidden_cursor(), term.fullscreen(), term.mouse_enabled(report_motion=False):
+    while True:
+        inp = term.inkey(timeout=0.2)
+        my = inp.mouse_xy[1]
+        if my > -1 and inp.name == 'MOUSE_LEFT':
+            if my == 1:
+                slots['slot1'] = not slots['slot1']
+            elif my == 2:
+                slots['slot2'] = not slots['slot2']
+            elif my == 3:
+                slots['slot3'] = not slots['slot3']
+            elif my == 4:
+                slots['slot4'] = not slots['slot4']
 
-    with term.cbreak(), term.fullscreen(), term.mouse_enabled(report_motion=False):
-        while True:
-            print(text, end='', flush=True)
-            inp = term.inkey()
-            if inp.name and inp.name.startswith("MOUSE_"):
-                with term.location(0,5):
-                    print(f"{inp.mouse_xy}".ljust(30))
-            time.sleep(0.01)
+        with term.location(0, 1):
+            for key in slots:
+                print(f'{key} : {slots[key]}'.ljust(30))
+
+        time.sleep(0.01)
