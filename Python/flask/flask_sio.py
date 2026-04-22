90bf2d7ad53bda796060795f700a93d7dc71e5dd
Sein-Oh <48703174+Sein-Oh@users.noreply.github.com>
2026-04-22 22:30:21 +0900

    Update flask_sio.py

--- Python/flask/flask_sio.py
+++ Python/flask/flask_sio.py
@@ -4,67 +4,61 @@ import os
 import threading
 import time
 
+import base64
+import cv2
+import numpy as np
+import json
+import requests
+
+import dxcam
+
+
 app = Flask(__name__)
 socketio = SocketIO(app, async_mode="threading")
 
 
-@app.route('/')
-def index():
-    HTML = """
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Flask-SocketIO</title>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/4.0.1/socket.io.js"></script>
-</head>
-<body>
-	<img id="img">
-    <textarea id="area" rows="6" cols="22"></textarea>
-</body>
-<script>
-	let t_prev = Date.now()
-	const img = document.getElementById("img")
-    const area = document.getElementById('area')
-	const socket = io()
-	socket.on("connect", () => {
-		console.log("connect")
-	})
-	
-	socket.on("time_event", (data) => {
-		console.log(data)
-        const value = `${data}\n`
-        area.append(value)
-	})
-	
-	socket.on("stream", (data) => {
-		img.src = `data:image/png;base64,${data.slice(2, -1)}`
-		t = Date.now()
-		//console.log(t-t_prev)
-		t_prev = t
-	})
-</script>
-</html>
-    """
-    return HTML
+def cv_to_b64(img):
+    ret, buffer = cv2.imencode(".png", img)
+    buffer_b = buffer.tobytes()
+    im_b64 = base64.b64encode(buffer_b)
+    return str(im_b64)
 
 
-def time_sender():
+def main_loop():
     while True:
+        frame = cam.get_latest_frame()
+        thumbnail = cv2.resize(frame, dsize=(0,0), fx=0.25, fy=0.25, interpolation=cv2.INTER_AREA)
+        thumbnail_b64 = cv_to_b64(thumbnail)
+        socketio.emit("stream", thumbnail_b64)
         now = time.strftime('%Y-%m-%d %H:%M:%S')
         socketio.emit("time_event", now)
-        #print(now)
-        time.sleep(1)
+        time.sleep(0.01)
+
+
+def run_server():
+    socketio.run(app, host="127.0.0.1", port="8000", debug=True, use_reloader=False)
+
+
+def open_browser():
+    os.system('start msedge --app=http://127.0.0.1:8000')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 
 @socketio.on('connect')
 def handle_connect():
     print('클라이언트가 연결되었습니다.')
-    threading.Thread(target=time_sender, daemon=True).start()
+    threading.Thread(target=main_loop, daemon=True).start()
     
+
 @socketio.on('disconnect')
 def handle_disconnect():
     print('클라이언트가 연결을 종료했습니다.')
     os._exit(1)
+
 
 @socketio.on('message')
 def handle_message(data):
@@ -72,4 +66,10 @@ def handle_message(data):
     socketio.emit('message', data)
 
 
+cam = dxcam.create(output_color="BGR")
+cam.start(target_fps=10)
+frame = cam.get_latest_frame()
+
+
+threading.Timer(0.1, open_browser).start()
 socketio.run(app, host="127.0.0.1", port="8000", debug=True, use_reloader=False)
