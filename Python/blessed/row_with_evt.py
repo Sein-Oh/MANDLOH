ac950102dc7d9774a8f951892f106adba93ce94b
Sein-Oh <48703174+Sein-Oh@users.noreply.github.com>
2026-04-29 16:30:19 +0900

    Create row_with_evt.py

---
+++ Python/blessed/row_with_evt.py
@@ -0,0 +1,46 @@
+from blessed import Terminal
+import time
+
+term = Terminal()
+
+
+def event_handler(event):
+    if event == 'COMMAND':
+        print(event)
+
+
+def draw(row, text, mouse_y, event=None):
+    with term.location(0, row):
+        print(text)
+    if mouse_y == row:
+        event_handler(event)
+
+
+def title(row, label, mouse_y, event=None):
+    inner_width = term.width - 1
+    with term.location(0, row):
+        print(term.white('─' * 2) + term.black_on_white(label) + term.white('─' * (inner_width - len(label) - 2)))
+    if mouse_y == row:
+        event_handler(event)
+
+
+try:
+    with term.cbreak(), term.hidden_cursor(), term.fullscreen(), term.mouse_enabled():
+        while True:
+            inp = term.inkey(timeout=0)
+            # Mouse
+            mouse_y = inp.mouse_xy[1] if inp.mouse_xy else -1
+            # Keyboard
+            pressed_key = inp.name if inp.is_sequence else inp
+            
+            title(0, 'HEADER', mouse_y)
+            draw(2, term.black_on_white('Hello world!!'), mouse_y)
+            draw(3, term.black_on_white('Hello world!!'), mouse_y, 'COMMAND')
+            time.sleep(0.01)
+
+except KeyboardInterrupt:
+    pass
+
+finally:
+    print(term.normal_cursor(), end='')
+    print(term.exit_fullscreen(), end='')
