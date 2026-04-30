21d82ff2d0dae9218dcc89c9032fd39be37fd397
Sein-Oh <48703174+Sein-Oh@users.noreply.github.com>
2026-04-30 10:53:14 +0900

    Update row_with_evt.py

--- Python/blessed/row_with_evt.py
+++ Python/blessed/row_with_evt.py
@@ -4,19 +4,39 @@ import time
 term = Terminal()
 
 
+def prompt(legend, msg):
+        print(term.clear)
+        text = ''
+        while True:
+            title(0, f' {legend} ')
+            draw(1, f'{msg} {text}'.ljust(term.width))
+            inp = term.inkey()
+            pressed_key = inp.name if inp.is_sequence else inp
+            if 'KEY' in pressed_key:
+                if pressed_key == 'KEY_BACKSPACE':
+                    text = text[:-1]
+                elif pressed_key == 'KEY_ENTER':
+                    print(term.clear)
+                    return text
+                elif pressed_key == 'KEY_EACAPE':
+                    return ''
+            else:                
+                text = text + pressed_key
+            
+
 def event_handler(event):
-    # with term.location(0, 10):
-    print(event.ljust(term.width))
+    if event == 'COMMAND':
+        print(event)
 
 
-def draw(row, text, mouse_y, event=""):
+def draw(row, text, mouse_y=-1, event=None):
     with term.location(0, row):
         print(text)
     if mouse_y == row:
         event_handler(event)
 
 
-def title(row, label, mouse_y, event=""):
+def title(row, label, mouse_y=-1, event=None):
     inner_width = term.width - 1
     with term.location(0, row):
         print(term.white('─' * 2) + term.black_on_white(label) + term.white('─' * (inner_width - len(label) - 2)))
@@ -29,13 +49,19 @@ try:
         while True:
             inp = term.inkey(timeout=0)
             # Mouse
-            mouse_y = inp.mouse_xy[1] if inp.name == 'MOUSE_LEFT' else -1
+            mouse_y = inp.mouse_xy[1] if inp.mouse_xy else -1
             # Keyboard
             pressed_key = inp.name if inp.is_sequence else inp
             
-            title(0, 'HEADER', mouse_y, 'HEADER')
-            draw(2, term.black_on_white('Hello world!!'), mouse_y, f'{mouse_y}')
-            draw(3, term.black_on_white('Hello world!!'), mouse_y, f'{mouse_y}')
+            
+            title(0, ' HEADER ', mouse_y, 'COMMAND')
+            draw(2, term.black_on_white('Hello world!!'), mouse_y)
+            draw(3, term.black_on_white('Hello world!!'), mouse_y, 'COMMAND')
+            
+            if pressed_key == 'KEY_F1':
+                rtn = prompt('PROMPT', 'Your message :')
+                print(rtn)
+                
             time.sleep(0.01)
 
 except KeyboardInterrupt:
